f3dbf642b8be09eeb40325550365222b6dfa3be1
Pavan <pavandeep2318@gmail.com>
2026-02-26 18:40:32 +0000

    fix: remove circular dependency by isolating prisma client

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const { PrismaClient } = require("@prisma/client");
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,18 +11,12 @@ app.get("/health", (req, res) => {
 });
 
 app.use("/register", require("./routes/register"));
-
 app.use("/login", require("./routes/login"));
-
-app.use("/log", require("./routes/log"));
-
 app.use("/phish", require("./routes/phish"));
-
+app.use("/log", require("./routes/log"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = { app, prisma };
