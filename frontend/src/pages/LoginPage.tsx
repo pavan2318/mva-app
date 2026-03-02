566a91e6110005ad5a40a8610e4077eef5c4b25a
Pavan <pavandeep2318@gmail.com>
2026-03-02 20:33:12 +0000

    feat: initial frontend scaffold (React + Tailwind)

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="p-8 bg-white rounded-xl shadow">
+        MVA Login Coming Online
+      </div>
+    </div>
+  )
+}
